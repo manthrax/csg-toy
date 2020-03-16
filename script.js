a609ae3e651e41aea73db527b9250d527372e5a1
Glitch (hello-webpage) <none>
2020-03-16 03:40:01 +0000

    🐟🍛 Checkpoint ./script.js:12955068/4923 ./index.html:12955068/421 ./style.css:12955068/269

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+let camera, scene, renderer;
+console.log(THREE);
 
+init();
+animate();
+
+function init() {
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
+
+  scene = new THREE.Scene();
+
+  const geometry = new THREE.PlaneBufferGeometry(2, 2);
+  const material = new THREE.MeshNormalMaterial();
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+  });
+  renderer.setClearColor(0x000000, 1);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
+
+  // const controls = new OrbitControls(camera, renderer.domElement);
+
+  onWindowResize();
+  window.addEventListener('resize', onWindowResize, false);
+}
+
+function onWindowResize(event) {
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);  
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  render(); 
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
