24432d2a0b6e81422ed50c3366caaeeb854d24ad
Glitch (hello-webpage) <none>
2020-07-08 12:33:33 +0000

    🌄🍢 Checkpoint ./fcad.js:10761630/3893

--- fcad.js
+++ fcad.js
@@ -2,83 +2,85 @@ import * as THREE from "https://threejs.org/build/three.module.js";
 
 import CSG from "./three-csg.js";
 
-class FNode extends CSG{
-  constructor(fcad){
+class FNode extends CSG {
+  constructor(fcad, type) {
+    super();
     this.fcad = fcad;
-    this.size = new THREE.Vector3(1,1,1)
-    this.scale = new THREE.Vector3(1,1,1)
-    this.position = new THREE.Vector3(0,0,0)
-    this.rotation = new THREE.Euler(0,0,0,'XYZ')
+    this.type = type;
+    this._size = new THREE.Vector3(1, 1, 1);
+    this._scale = new THREE.Vector3(1, 1, 1);
+    this._position = new THREE.Vector3(0, 0, 0);
+    this._rotation = new THREE.Euler(0, 0, 0, "XYZ");
   }
-  size(x,y,z){
-    this.size.set(x,y,z)
+  size(x, y, z) {
+    this.size.set(x, y, z);
     return this;
   }
-  scale(x,y,z){
-    this.scale.set(x,y,z)
+  scale(x, y, z) {
+    this.scale.set(x, y, z);
     return this;
   }
-  position(x,y,z){
-    this.position.set(x,y,z)
+  position(x, y, z) {
+    this.position.set(x, y, z);
     return this;
   }
-  rotation(x,y,z){
-    this.rotation.set(x,y,z)
+  rotation(x, y, z) {
+    this.rotation.set(x, y, z);
     return this;
   }
-  get mesh(){
-    return CSG.fromMesh(this.src)
+  get mesh() {
+    return CSG.fromMesh(this.src);
   }
-  set mesh(src){
-    this.src=src;
+  set mesh(src) {
+    this.src = src;
   }
 }
 
 class FCAD {
   constructor(scene) {
     this.scene = scene;
-    this.elements=[]
-    let vec3=(x,y,z)=>new THREE.Vector3(x,y,z)
-    let sphere=()=>{
-      let fn = new FNode(this,'sphere')
+    this.elements = [];
+    let vec3 = (x, y, z) => new THREE.Vector3(x, y, z);
+
+    let sphere = () => {
+      let fn = new FNode(this, "sphere");
       return fn;
-    }
-    let box=()=>{      
-      let fn = new FNode(this,'box')
+    };
+    let box = () => {
+      let fn = new FNode(this, "box");
       return fn;
-    }
-    let cylinder=()=>{
-      let fn = new FNode(this,'cylinder')
+    };
+    let cylinder = () => {
+      let fn = new FNode(this, "cylinder");
       return fn;
-    }
-    let hull=(a)=>{
+    };
+    let hull = a => {
       return this;
-    }
-    let union=(a)=>{
+    };
+    let union = a => {
       return this;
-    }
-    let subtract=(a)=>{
+    };
+    let subtract = a => {
       return this;
-    }
-    let intersect=(a)=>{
+    };
+    let intersect = a => {
       return this;
-    }
-    let invert=(a)=>{
+    };
+    let invert = a => {
       return this;
-    }
-    let recompute=()=>{
-      debugger
-      return this
-    }
-    let feval=(str)=>{
+    };
+    let render = () => {
+      
       return this;
-    }
-    this.eval = feval
-    let f=`
-    scene.add(union(sphere(),box()))
-    `
-    this.eval(f)
+    };
+    this.eval = str => {
+      eval(str);
+      return this;
+    };
+    let f = `
+      render(sphere().size(1,1,1),box().size(1,1,1).position(.5,.5,.5))
+    `;
+    this.eval(f);
   }
 }
 export default FCAD;
-
