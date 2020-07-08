8e948fc22f2f60c351011f843b6ede4d5cc5ec5a
Glitch (hello-webpage) <none>
2020-07-08 05:21:26 +0000

    🎐🏞 Checkpoint ./app3.js:10761630/529

--- app3.js
+++ app3.js
@@ -2,7 +2,7 @@ import * as THREE from "https://threejs.org/build/three.module.js";
 import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 import { TransformControls } from "https://threejs.org/examples/jsm/controls/TransformControls.js";
 import GridMaterial from "./grid-material.js";
-let camera, scene, renderer, controls;
+let camera, scene, renderer, ocontrols;
 let aspect = window.innerWidth / window.innerHeight;
 camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 camera.position.set(0, 5, 10);
@@ -10,13 +10,13 @@ scene = new THREE.Scene();
 renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setClearColor(0x101010);
 document.body.appendChild(renderer.domElement);
-controls = new OrbitControls(camera, renderer.domElement);
-//controls.autoRotate = true;
-const geometry = new THREE.BoxBufferGeometry(5, 5, 5, 1, 1);
+ocontrols = new OrbitControls(camera, renderer.domElement);
+//ocontrols.autoRotate = true;
+const geometry = new THREE.BoxBufferGeometry(1,1,1, 1, 1);
 const material = new THREE.MeshStandardMaterial();
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
-mesh.position.y += 2.5;
+mesh.position.y += 0.5;
 const light = new THREE.PointLight("white", 0.5);
 light.position.set(20, 30, 40);
 scene.add(light);
@@ -24,6 +24,11 @@ const light1 = new THREE.PointLight("white", 0.5);
 light1.position.set(-20, 30, -40);
 scene.add(light1);
 
+let tcontrol = new TransformControls( camera, renderer.domElement );
+tcontrol.attach( mesh );
+scene.add(tcontrol)
+
+tcontrol.addEventListener( 'dragging-changed', event => ocontrols.enabled = ! event.value);
 let grid = new THREE.Mesh(new THREE.PlaneGeometry(10, 10),new GridMaterial());
 grid.rotation.x = Math.PI * -0.5;
 scene.add(grid);
@@ -43,6 +48,6 @@ for (let t = 0; t < 100; t++) {
 }
 
 renderer.setAnimationLoop(() => {
-  controls.update();
+  ocontrols.update();
   renderer.render(scene, camera);
 });
