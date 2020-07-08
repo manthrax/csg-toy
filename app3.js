525ec228591ed468564d9bc2d8626b60faba7583
Glitch (hello-webpage) <none>
2020-07-08 05:09:27 +0000

    🍜🕌 Checkpoint ./app3.js:10761630/377 ./grid-material.js:10761630/743

--- app3.js
+++ app3.js
@@ -1,16 +1,17 @@
 import * as THREE from "https://threejs.org/build/three.module.js";
 import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
+import { TransformControls } from "https://threejs.org/examples/jsm/controls/TransformControls.js";
 import GridMaterial from "./grid-material.js";
 let camera, scene, renderer, controls;
 let aspect = window.innerWidth / window.innerHeight;
 camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
-camera.position.set(0, 0, 10);
+camera.position.set(0, 5, 10);
 scene = new THREE.Scene();
 renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setClearColor(0x101010);
 document.body.appendChild(renderer.domElement);
 controls = new OrbitControls(camera, renderer.domElement);
-controls.autoRotate = true;
+//controls.autoRotate = true;
 const geometry = new THREE.BoxBufferGeometry(5, 5, 5, 1, 1);
 const material = new THREE.MeshStandardMaterial();
 const mesh = new THREE.Mesh(geometry, material);
@@ -23,7 +24,7 @@ const light1 = new THREE.PointLight("white", 0.5);
 light1.position.set(-20, 30, -40);
 scene.add(light1);
 
-let grid = new THREE.Mesh(new THREE.PlaneGeometry(10, 10));
+let grid = new THREE.Mesh(new THREE.PlaneGeometry(10, 10),new GridMaterial());
 grid.rotation.x = Math.PI * -0.5;
 scene.add(grid);
 
--- grid-material.js
+++ grid-material.js
@@ -1,27 +1,38 @@
 import * as THREE from "https://threejs.org/build/three.module.js";
-
-let fs = `
-// License: CC0 (http://creativecommons.org/publicdomain/zero/1.0/)
-#extension GL_OES_standard_derivatives : enable
+let vs = `
 varying vec3 vertex;
 void main() {
-  vec2 coord = vertex.xz;// Compute anti-aliased world-space grid lines
+  vertex = position;
+	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}`
+let fs = `
+// License: CC0 (http://creativecommons.org/publicdomain/zero/1.0/)
+//#extension GL_OES_standard_derivatives : enable
+varying vec3 vertex;
+uniform vec4 color;
+void main() {
+  vec2 coord = vertex.xy;// Compute anti-aliased world-space grid lines
   vec2 grid = abs(fract(coord - 0.5) - 0.5) / fwidth(coord);
   float line = min(grid.x, grid.y);
-  gl_FragColor = vec4(vec3(1.0 - min(line, 1.0)), 1.0);// Just visualize the grid lines directly
+  float val =  1.0 - min(line, 1.0);
+  gl_FragColor = vec4(color.xyz, color.w*val);// Just visualize the grid lines directly
 }
 `;
 
 class GridMaterial extends THREE.ShaderMaterial {
-  constructor() {
+  constructor(params) {
+    //ebugger
     super({
       uniforms: {
         time: { value: 1.0 },
-        resolution: { value: new THREE.Vector2() }
+        resolution: { value: new THREE.Vector2() },
+        color: { value: new THREE.Vector4(1,1,0,1) }
       },
-      //vertexShader: document.getElementById( 'vertexShader' ).textContent,
+      vertexShader:vs,
       fragmentShader: fs,
-      extensions:{derivatives:true}
+      extensions:{derivatives:true},
+      transparent:true,
+      side:THREE.DoubleSide
     });
   }
 }
