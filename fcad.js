9af95234ea90e89b5fdc1d9390faa685024f17c5
Glitch (hello-webpage) <none>
2020-07-12 03:38:16 +0000

    🎬👨‍👨‍👦 Checkpoint ./fcad.js:10761630/19548

--- fcad.js
+++ fcad.js
@@ -28,107 +28,101 @@ class FNode {
     this._rotation.set(x, y, z, order);
     return this;
   }
-
   getMesh() {
     return Prims[this.type](this);
   }
 }
 
 class Prims {
-  static bindNodeToMesh(e,m){
-    m.position.copy(e._position)
-    m.scale.copy(e._scale)
-    m.rotation.copy(e._rotation)
+  static bindNodeToMesh(e, m) {
+    m.position.copy(e._position);
+    m.scale.copy(e._scale);
+    m.rotation.copy(e._rotation);
     m.userData.node = e;
-    return m
+    return m;
   }
   static mesh(e, geometry, material = frontMaterial) {
     let m = new THREE.Mesh(geometry, material);
-    Prims.bindNodeToMesh(e,m)
-    return m
+    Prims.bindNodeToMesh(e, m);
+    return m;
   }
-  static empty(e,material) {
-    return this.mesh(e, new THREE.Geometry(),material);
+  static empty(e, material) {
+    return this.mesh(e, new THREE.Geometry(), material);
   }
-  static sphere(e,material) {
-    return this.mesh(e, new THREE.SphereGeometry(.5, 16, 16),material);
+  static sphere(e, material) {
+    return this.mesh(e, new THREE.SphereGeometry(0.5, 16, 16), material);
   }
-  static box(e,material) {
-    return this.mesh(e, new THREE.BoxGeometry(1,1,1),material);
+  static box(e, material) {
+    return this.mesh(e, new THREE.BoxGeometry(1, 1, 1), material);
   }
-  static cylinder(e,material) {
-    return this.mesh(e, new THREE.CylinderGeometry(0.5, 0.5, 1, 16),material);
+  static cylinder(e, material) {
+    return this.mesh(e, new THREE.CylinderGeometry(0.5, 0.5, 1, 16), material);
   }
   static union(e) {
-    if(e.args.length){
-      //debugger
-      let p = Prims.empty(e,csgMaterial)
-      var bspA = CSG.fromMesh( p );
-      e.args.forEach((b,i)=>{if(!i)return
-        let bspB = CSG.fromMesh( b.getMesh() );
-        bspA = bspA.union(bspB)
-      })
-      return Prims.bindNodeToMesh(e,CSG.toMesh( bspA, p.matrix,csgMaterial));
+    debugger
+    if (e.args.length) {
+      let p = Prims.empty(e, csgMaterial);
+      var bspA = CSG.fromMesh(p);
+      e.args.forEach((b, i) => {
+        if (!i) return;
+        let bspB = CSG.fromMesh(b.getMesh());
+        bspA = bspA.union(bspB);
+      });
+      return Prims.bindNodeToMesh(e, CSG.toMesh(bspA, p.matrix, csgMaterial));
     }
-    return Prims.sphere(e,csgMaterial);
+    return Prims.sphere(e, csgMaterial);
   }
   static subtract(e) {
-    return Prims.sphere(e,csgMaterial);
+    return Prims.sphere(e, csgMaterial);
   }
   static intersect(e) {
-    return Prims.sphere(e,csgMaterial);
+    return Prims.sphere(e, csgMaterial);
   }
   static invert(e) {
-    return Prims.sphere(e,csgMaterial);
+    return Prims.sphere(e, csgMaterial);
   }
 }
 
-let empty = new THREE.Object3D()
+let empty = new THREE.Object3D();
 class FCAD {
-  toJSON()
-  {
-    let id=0;
-    this.nodes.forEach(n=>n.id=id++)
-    let out=[]
-    this.nodes.forEach(n=>{
-      if(n.args){
-        let ids=[]
-        n.args.forEach((a,i)=>ids.push(a.id))
-        n.iargs=ids;
+  toJSON() {
+    let id = 0;
+    this.nodes.forEach(n => (n.id = id++));
+    let out = [];
+    this.nodes.forEach(n => {
+      if (n.args) {
+        let ids = [];
+        n.args.forEach((a, i) => ids.push(a.id));
+        n.iargs = ids;
       }
-    })
-    this.nodes.forEach(n=>{
-      let o={}
-      o.type=n.type
+    });
+    this.nodes.forEach(n => {
+      let o = {};
+      o.type = n.type;
       n.name && (o.name = n.name);
-      n.args && n.args.length && (o.args=n.iargs);
-      (!n._position.equals(empty.position)) && (o.position=n._position);
-      (!n._scale.equals(empty.scale)) && (o.scale=n._scale);
-      (!n._rotation.equals(empty.rotation)) && (o.rotation=n._rotation);
-      out.push(o)
-    })
+      n.args && n.args.length && (o.args = n.iargs);
+      !n._position.equals(empty.position) && (o.position = n._position);
+      !n._scale.equals(empty.scale) && (o.scale = n._scale);
+      !n._rotation.equals(empty.rotation) && (o.rotation = n._rotation);
+      out.push(o);
+    });
     return out;
   }
-  fromJSON(js){
-    js.forEach((e,i)=>this.nodes.push(new FNode(this,e.type)))
-    for(let i=0;i<js.length;i++)
-    js.forEach((e,i)=>{
-      e.args && e.args.forEach((a,ai)=>e.args[ai]=this.nodes[a])
-      e.position && this.nodes[i]._position.copy(e.position)
-      e.scale && this.nodes[i]._scale.copy(e.scale)
-      e.rotation && this.nodes[i]._rotation.copy(e.rotation)
-    })
+  fromJSON(js) {
+    js.forEach((e, i) => this.nodes.push(new FNode(this, e.type)));
+    js.forEach((e, i) => {
+      e.args && e.args.forEach((a, ai) => (e.args[ai] = this.nodes[a]));
+      e.position && this.nodes[i]._position.copy(e.position);
+      e.scale && this.nodes[i]._scale.copy(e.scale);
+      e.rotation && this.nodes[i]._rotation.copy(e.rotation);
+    });
   }
-  
-  
+
   constructor(scene) {
     this.scene = scene;
     this.nodes = [];
     this.elements = [];
 
-    
-    
-    
     let addNode = node => {
       this.nodes.push(node);
       return node;
@@ -136,8 +130,7 @@ class FCAD {
     function nnode(type, args) {
       return addNode(new FNode(self, type, Array.prototype.slice.call(args)));
     }
-    
-    
+
     let self = this;
     function render() {
       self.elements = [];
@@ -147,66 +140,62 @@ class FCAD {
       }
       return self;
     }
-    
-let mkDefault=()=>{
-    let vec3 = (x, y, z) => new THREE.Vector3(x, y, z);
-    let sphere = () => addNode(new FNode(this, "sphere"));
-    let box = () => addNode(new FNode(this, "box"));
-    let cylinder = () => addNode(new FNode(this, "cylinder"));
+    this.update = () => {
+      render(...this.nodes);
+      localStorage.csgscene = JSON.stringify(this.toJSON());
+      return this.elements;
+    };
 
-    let mesh = function() {
-      return nnode("mesh", arguments);
+    let mkDefault = () => {
+      let vec3 = (x, y, z) => new THREE.Vector3(x, y, z);
+      let sphere = () => addNode(new FNode(this, "sphere"));
+      let box = () => addNode(new FNode(this, "box"));
+      let cylinder = () => addNode(new FNode(this, "cylinder"));
+
+      let mesh = function() {
+        return nnode("mesh", arguments);
+      };
+      let hull = function() {
+        return nnode("hull", arguments);
+      };
+      let union = function() {
+        return nnode("union", arguments);
+      };
+      let subtract = function() {
+        return nnode("subtract", arguments);
+      };
+      let intersect = function() {
+        return nnode("intersect", arguments);
+      };
+      let invert = function() {
+        return nnode("invert", arguments);
+      };
+
+      let a = box()
+        .size(1, 1, 1)
+        .position(1, 0.5, 1);
+
+      let b = cylinder()
+        .size(1, 1, 1)
+        .position(2, 0.5, 2);
+
+      let c = sphere()
+        .size(1, 1, 1)
+        .position(3, 0.5, 2);
+
+      let u = union(a, b);
+
+      this.update = () => {
+        return render(a, b, c, u).elements;
+      };
     };
-    let hull = function() {
-      return nnode("hull", arguments);
-    };
-    let union = function() {
-      return nnode("union", arguments);
-    };
-    let subtract = function() {
-      return nnode("subtract", arguments);
-    };
-    let intersect = function() {
-      return nnode("intersect", arguments);
-    };
-    let invert = function() {
-      return nnode("invert", arguments);
+    try {
+      this.fromJSON(JSON.parse(localStorage.csgscene));
+    } catch {
+      mkDefault();
     }
-
-    let a = box()
-      .size(1, 1, 1)
-      .position(1,.5,1);
-
-    let b = cylinder()
-      .size(1, 1, 1)
-      .position(2,.5,2);
-
-    let c = sphere()
-      .size(1, 1, 1)
-      .position(3,.5,2);
-
-    let u = union(a, b)
-    
-
-    this.update = () => {
-      return render(a, b, c, u ).elements;
-    };
-}
-try
-{
-  //throw ""
-  this.fromJSON(JSON.parse(localStorage.csgscene))
-    this.update = () => {
-      return render( ...this.nodes ).elements;
-    };
-}
-catch
-{
-  mkDefault();//this.update=()=>this.elements
-}
-    this.update(); 
-    console.log(this.toJSON())
-    localStorage.csgscene = JSON.stringify(this.toJSON())
+    this.update();
+    localStorage.csgscene = JSON.stringify(this.toJSON());
   }
 }
 export default FCAD;
@@ -231,102 +220,3 @@ const csgMaterial = new THREE.MeshStandardMaterial({
   transparent: false,
   side: THREE.FrontSide
 });
-/*
-
-[
-  {name:'a',type:'sphere'}
-]
-
-     
-    //var mesh = new THREE.Mesh( new THREE.ConvexBufferGeometry(points ));
-    
-    
-    let doOp = el => {
-      let t = el.type;
-      el.csg = new CSG();
-      if (t === "union") {
-        //debugger
-        if(el.args.length){el.src = el.args[0].getMesh(); el.csg = CSG.fromMesh(el.src);}
-        for (let i = 1; i < el.args.length; i++)
-          el.csg = el.csg.union(el.args[i].csg);
-        el._mesh = el.getMesh();
-        el._mesh.material = new THREE.MeshStandardMaterial();
-      }
-    };
-
-    function render() {
-      while (scene.children.length) scene.remove(scene.children[0]);
-      self.elements.length = 0;
-      for (let e = arguments, i = 0; i < e.length; i++) {
-        let el = e[i];
-        let t = el.type;
-        if (prims[t]) {
-          let p = prims[t].clone(true)
-          p.position.copy(el._position);
-          p.scale.copy(el._scale);
-          p.rotation.copy(el._rotation);
-          el.setMesh(p);
-          el._mesh = el.getMesh();
-        } else doOp(el);
-        self.elements.push(el._mesh);
-        scene.add(el._mesh);
-      }
-      return self;
-    }
-
-    this.eval = str => {
-      eval(str);
-      return this;
-    };
-
-getMesh() {
-    this.src.updateMatrixWorld();
-    let m = CSG.toMesh(this.csg, this.src.matrix, this.src.material);
-    m.updateMatrixWorld();
-    m.renderOrder = 2;
-    let b = new THREE.Mesh(m.geometry, backMaterial);
-    m.add(b);
-    b.renderOrder = 1;
-    m.userData.node = this;
-    return this.mesh = m;
-  }
-  setSrc(src) {
-    this.src = src;
-    this.csg = CSG.fromMesh(this.src);
-    return this.src;
-  }
-*/
-/*
-    let f = `
-let s = sphere().size(1,1,1).position(.15, 0.25, -.15)
-let b = box().size(1,1,1).position(.15, 0.25, .25)
-let c = cylinder().size(1,1,1).position(.15, 0.25, -.35)
-let u = union(b,s,c)
-      render(u,b,s,c)
-    `;
-
-    this.eval(f);
-*/
-// debugger
-/*
-    let s = sphere()
-      .size(1.5, 1.5, 1.5)
-      .position(0.15, 0.25, -0.15);
-    let b = box()
-      .size(1, 1, 1)
-      .position(0.15, 0.25, 0.25);
-    let c = cylinder()
-      .size(1, 1, 1)
-      .position(0.15, 0.25, -0.35);
-   //let u = union(s, c);
-*/
-
-/*    render(
-      sphere().size(1, 1, 1)
-        .position(.15, 0.5, -.15),
-      box()
-        .size(1, 1, 1)
-        .position(.15, .5, .15),
-      cylinder()
-    );
-*/
